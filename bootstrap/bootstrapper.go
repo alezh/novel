483c06b8c5d7aaf1b52214acb3cbbf5a770e760c
alezh <watman_liu@163.com>
2018-04-13 11:19:13 +0800

    update

--- bootstrap/bootstrapper.go
+++ bootstrap/bootstrapper.go
@@ -88,7 +88,7 @@ func (b *Bootstrapper) SetupErrorHandlers() {
 
 const (
 	// StaticAssets is the root directory for public assets like images, css, js.
-	StaticAssets = "./public/"
+	StaticAssets = "../github.com/alezh/novel/public/"
 	// Favicon is the relative 9to the "StaticAssets") favicon path for our app.
 	Favicon = "favicon.ico"
 )
@@ -104,7 +104,7 @@ func (b *Bootstrapper) Configure(cs ...Configurator) {
 //
 // Returns itself.
 func (b *Bootstrapper) Bootstrap() *Bootstrapper {
-	b.SetupViews("./views",false)
+	b.SetupViews("../github.com/alezh/novel/views",false)
 	b.SetupSessions(24*time.Hour,
 		[]byte("the-big-and-secret-fash-key-here"),
 		[]byte("lot-secret-of-characters-big-too"),
