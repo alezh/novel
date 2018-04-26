08412e02811198fd5e7404d14f5d26035903b3aa
alezh <watman_liu@163.com>
2018-04-26 18:15:58 +0800

    Initialization

--- system/mission.go
+++ system/mission.go
@@ -1,21 +1,8 @@
 package system
 
-import (
-	"github.com/alezh/novel/modules/spider"
-	"github.com/alezh/novel/system/http/request"
-	"net/http"
-	"github.com/PuerkitoBio/goquery"
-	"sync"
-)
 
 //任务调度与分配
 
 type Mission struct {
-	spider spider.Spider
-	Request  *request.Request  //连接请求
-	Response *http.Response    // 响应流
-	text     []byte            // 下载内容Body的字节流格式
-	dom      *goquery.Document // 下载内容Body为html时，可转换为Dom的对象
-	err      error             // 错误标记
-	sync.Mutex
+
 }
